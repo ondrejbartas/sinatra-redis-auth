bc32f9f59a6521015d1647c6487416d970219fa3
Ondrej Bartas <ondrej@bartas.cz>
2012-03-06 23:55:53 +0100

    [TEST] Add test file

---
+++ test/test_sinatra-redis-auth.rb
@@ -0,0 +1,11 @@
+# -*- encoding : utf-8 -*-
+require 'helper'
+class SinatraRedisAuthTest < Test::Unit::TestCase
+  context "RedisModel" do
+    setup do
+    end
+    should "Test" do
+      assert true
+    end
+  end
+end
